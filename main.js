70fb40af887227faf77f834bb697f2e508e41655
meccacodes <hi@windfallyarn.com>
2024-11-04 11:12:12 -0800

    Created moveDisk function, currently logging values of fromTower and toTower inputs when submit button is clicked

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// Grab HTML elements
+const fromTower = document.querySelector(".fromTower");
+const toTower = document.querySelector(".toTower");
+const moveButton = document.querySelector(".moveButton");
+const disks = [
+  document.querySelector(".disk-1"),
+  document.querySelector(".disk-2"),
+  document.querySelector(".disk-3"),
+  document.querySelector(".disk-4"),
+  document.querySelector(".disk-5"),
+];
+
 // A board representing 3 towers, and 5 discs on the first tower in ascending order.
 
 let startingBoard = [[5, 4, 3, 2, 1], [], []];
@@ -10,13 +22,6 @@ let boardState = {
 
 // add tower1 array values onto disks
 document.addEventListener("DOMContentLoaded", () => {
-  const disks = [
-    document.querySelector(".disk-1"),
-    document.querySelector(".disk-2"),
-    document.querySelector(".disk-3"),
-    document.querySelector(".disk-4"),
-    document.querySelector(".disk-5"),
-  ];
 
   // Reverse the order of the disks array
   const reversedDisks = disks.reverse();
@@ -33,6 +38,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// Move top disk from fromTower to toTower in DOM
+const moveDisk = function (fromTower, toTower) {
+  console.log(`Move disc from tower ${fromTower} to tower ${toTower}`);
+};
+
 // player submits a move
 
 const submitButton = document.querySelector("#moveButton");
@@ -45,7 +55,13 @@ submitButton.addEventListener("click", function (event) {
   console.log(fromTower);
   let toTower = document.querySelector(".toTower").value;
   console.log(toTower);
+  moveDisk(fromTower, toTower);
+  // return fromTower and toTower values
+  return (fromTower, toTower)
 });
+
+
+
 
 // check if move is valid:
 // 1. check if disc can be moved (must be on top)
@@ -54,9 +70,6 @@ submitButton.addEventListener("click", function (event) {
 // if not valid, display error message
 let validateMove = function (fromTower, toTower) {};
 
-let moveDisc = function (fromTower, toTower) {
-  console.log(`Move disc from tower ${fromTower} to tower ${toTower}`);
-};
 
 // //////////////////////////////////////////////////////////////////////////////
 // *** SANDBOX ***
